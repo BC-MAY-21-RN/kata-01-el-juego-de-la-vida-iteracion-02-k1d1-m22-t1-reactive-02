98b18962c36a2ce9084a9f7fa32ce71696ee9f6e
Gerardo Tovar <geemtovarba@ittepic.edu.mx>
2022-05-04 18:19:34 -0600

    show next generation matrix Co-authored-by: name <noel.silva.v@gmail.com> Co-authored-by: name <nicolasjrz.dev@gmail.com>

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ class Matrix {
         this._matrix = this._generateRandom();
     }
     _generateRandom() {
-        let matrix = new Array(altura).fill(new Array(anchura).fill('')).map(fila => fila.map(_ => 0));
+        let matrix = new Array(this._altura).fill(new Array(this._anchura).fill('')).map(fila => fila.map(_ => 0));
         for (let fi = 1; fi < matrix.length - 1; fi++) {
             for (let ci = 1; ci < matrix[fi].length - 1; ci++) {
                 matrix[fi][ci] = Math.round(Math.random());
             }
         }
         return matrix;
-        
     }
     setAlive(x, y) {
         this._matrix[x][y] = 1
@@ -23,14 +22,21 @@ class Matrix {
     setAltura(altura) {
         this.altura = altura
     }*/
+    getMatrix() {
+        return this._matrix;
+    }
+    setMatrix(matrix) {
+        this._matrix = matrix;
+    }
     getAnchura() {
         return this._anchura;
     }
     getAltura() {
         return this._altura;
     }
-    
-    
+    show() {
+        console.log(this._matrix.map(f => f.join(' ')).join('\n'))
+    }
 }
 
 
@@ -39,7 +45,6 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
 
 function CoordX(max, resolver) {
     return new Promise((resolve) => {
@@ -52,7 +57,7 @@ function CoordX(max, resolver) {
         });
     });
 }
-11
+
 function CoordY(max, resolver) {
     return new Promise((resolve) => {
         rl.question('Coordenada y => ', function (data) {
@@ -91,7 +96,8 @@ async function main() {
                 matrix.setAlive(y, x);
                 break;
             case '2':
-                console.log(matrix);
+                matrix.show();
+                matrix.setMatrix(nextGeneration(matrix));
                 break;
             default:
 
@@ -99,31 +105,57 @@ async function main() {
         console.log("desea continuar s/n:");
         char = await askOption();
     }
-    
- 
-
-
-
-
-
-    
-    // let matrix = new Matrix(altura, anchura)
-    // console.log(matrix)
-    // SecondPeticion();
-    // makeMatrix();
 }
+
+function nextGeneration(matrix){
+    let aux = new Array(matrix.getAltura()).fill(new Array(matrix.getAnchura()).fill('')).map(fila => fila.map(_ => 0));
+    
+    for (let x = 1; x < matrix.getMatrix().length - 1; x++) {
+        for (let y = 1; y < matrix.getMatrix()[x].length -1; y++) {
+            let VecinosVivos = 0;
+            if (matrix.getMatrix()[x-1][y-1] == 1) VecinosVivos++;
+            if (matrix.getMatrix()[x][y-1] == 1) VecinosVivos++;
+            if (matrix.getMatrix()[x+1][y-1] == 1) VecinosVivos++;
+            
+            if (matrix.getMatrix()[x-1][y] == 1) VecinosVivos++;
+            if (matrix.getMatrix()[x+1][y] == 1) VecinosVivos++;
+            
+            if (matrix.getMatrix()[x-1][y+1] == 1) VecinosVivos++;
+            if (matrix.getMatrix()[x][y+1] == 1) VecinosVivos++;
+            if (matrix.getMatrix()[x+1][y+1] == 1) VecinosVivos++;
+            // console.log(VecinosVivos);
+            if (matrix.getMatrix()[x][y] == 0){
+                if (VecinosVivos == 3) aux[x][y] = 1;
+            } else {
+                if (VecinosVivos == 2 || VecinosVivos == 3){
+                    aux[x][y] = 1;
+                }
+            }
+        }
+    }
+    
+    return aux
+} 
 /*
 x > 0
 y > 0
 y < max
 x < max
+(x=x-1, y=y-1) (x=x, y=y-1) (x=x+1, y=y-1)
+(x=x-1, y=y)                (x=x+1, y=y)
+(x=x-1, y=y+1) (x=x, y=y+1) (x=x+1, y=y+1)
+
 ["00"],["01"],["02"],["03"],["04"],
 ["10"],["11"],["12"],["13"],["14"],
 ["20"],["21"],["22"],["23"],["24"],
 ["30"],["31"],["32"],["33"],["44"],
 ["40"],["41"],["42"],["43"],["44"],
 
-
+0 0 0 0 0
+0 1 1 1 0
+0 0 0 1 0
+0 0 0 0 0
+0 0 0 0 0
 */
 
 
